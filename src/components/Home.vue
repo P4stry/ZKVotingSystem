b03a64e001866c7eaaa70f2ea0f73f181740cefb
Zhong Juantao <1340636752@qq.com>
2024-05-27 22:03:00 +0800

    Update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,10 +4,11 @@
     <div class="row justify-content-md-center">
       <div class="col-lg-4">
         <div class="text-center vstack gap-3">
-          <h1>Scalable Voting System</h1>
-          <h2>I'm a</h2>
-          <a href="#/validator" class="btn btn-primary">Validator</a>
-          <a href="#/voter" class="btn btn-primary">Voter</a>
+          <h1>Simple voting system</h1>
+          <a href="#/validator" class="btn btn-primary">Validator tool</a>
+          <a href="#/registration" class="btn btn-primary">Registration to vote</a>
+          <a href="#/vote" class="btn btn-primary">Vote</a>
+          <a href="#/results" class="btn btn-primary">Results</a>
         </div>
       </div>
     </div>
