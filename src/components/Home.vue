ec4f07447e3aea23a8b7a6baeee02866448fcd36
Zhong Juantao <1340636752@qq.com>
2024-05-28 17:08:11 +0800

    Update front-end

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,16 +5,29 @@
       <div class="col-lg-4">
         <div class="text-center vstack gap-3">
           <h1>Scalable Voting System</h1>
-            <div class = "col justify-content-md-center">
-              <h2>I'm a</h2>
-              <a href="#/validator" class="btn btn-primary">Validator</a>
-              <a href="#/voter" class="btn btn-primary">Voter</a>
-            </div>
+            <div class = "content">
+              <h2 class = "element">I'm a</h2>
+              <a href="#/validator" class="btn btn-primary element">Validator</a>
+              <a href="#/voter" class="btn btn-primary element">Voter</a>
+          </div>
         </div>
       </div>
     </div>
   </main>
 </template>
+
+.content {
+  display: table;
+  height: 100%;
+  width: 100%;
+}
+
+.element {
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+}
+
 
 <script lang="ts">
 import { Component, Vue } from "vue-facing-decorator";
