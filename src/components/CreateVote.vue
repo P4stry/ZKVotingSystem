3948446b2a4ef4737b5d30e06609883e4e7c75c0
Zhong Juantao <1340636752@qq.com>
2024-05-28 19:34:24 +0800

    Update

--- src/components/CreateVote.vue
+++ src/components/CreateVote.vue
@@ -5,17 +5,19 @@
       <div class="col-lg-4">
         <div class="text-center vstack gap-3">
           <h1>Create new Vote Proposal</h1>
+          <h2>Enter the voting duration: </h2>
+          <input type="number" placeholder="Integer from 1 to 100" v-model="duration" min="1" required />
           <h2>Enter the number of options: </h2>
           <input type="number" placeholder="Integer from 1 to 100" v-model="number" min="1" required />
           <button class="btn btn-info" @click="generateInputBoxes">Confirm</button>
           <input
+            placeholder="Option"
             v-for="(value, index) in inputBoxes"
             :key="index"
             v-model="inputBoxes[index]"
             type="text"
-            class="inputBox"
           />
-          <button class="btn btn-info" @click="logValues">Create</button>
+          <button class="btn btn-info" @click="createVote">Create</button>
         </div>
       </div>
     </div>
@@ -24,9 +26,11 @@
   
 <script lang="ts">
 import { Component, Vue } from "vue-facing-decorator";
+import * as ethers from "ethers";
 
 @Component
 export default class CreateVote extends Vue {
+  duration = 0;
   number = 0;
   inputBoxes = [];
 
@@ -34,8 +38,11 @@ export default class CreateVote extends Vue {
     this.inputBoxes = Array.from({ length: this.number }, (_, i) => i);
   }
 
-  logValues() {
+  async createVote() {
+    // const VotingSystem = await ethers.getContractFactory("VotingSystem");
+    // const votingSystem = await VotingSystem.deploy(TREE_LEVELS, mimcsponge.address, verifier.address, 4, 4);
     console.log("Recorded values:", this.inputBoxes);
+    console.log("Duration:", this.duration);
     this.inputBoxes.forEach((value, index) => {
       console.log(`Option ${index + 1}: ${value}`);
     });
