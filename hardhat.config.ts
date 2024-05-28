e27c8db93c21017be5bf6238c3535d6ce7b28502
Zhong Juantao <1340636752@qq.com>
2024-05-28 14:32:17 +0800

    Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,4 @@
-// import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from 'dotenv';
 dotenv.config();
@@ -6,16 +6,18 @@ dotenv.config();
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-// const config: HardhatUserConfig = {
-//   solidity: "0.8.17",
-// };
-
-export default {
+const config: HardhatUserConfig = {
   solidity: "0.8.17",
-  networks: {
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-    }
-  }
 };
+
+export default config;
+
+// export default {
+//   solidity: "0.8.17",
+//   networks: {
+//     sepolia: {
+//       url: SEPOLIA_RPC_URL,
+//       accounts: [PRIVATE_KEY],
+//     }
+//   }
+// };
